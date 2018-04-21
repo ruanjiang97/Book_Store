280e17e77c695a6adfe317c53f65edb593ced2c7
HGiang02 <37242751+HGiang02@users.noreply.github.com>
2018-04-21 19:57:20 +0700

    Add files via upload
    thêm side_bar_user_info

---
+++ resources/views/layout/elements/side_bar_user_information.blade.php
@@ -0,0 +1,31 @@
+<div class="left-sidebar">
+	<br>
+	<br>
+	<h2>Tài khoản</h2>
+	<div class="panel-group category-products" id="accordian"><!--category-productsr-->
+		
+
+		
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h4 class="panel-title"><a href="#">Thông tin tài khoản</a></h4>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h4 class="panel-title"><a href="#">Đơn hàng của tôi</a></h4>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h4 class="panel-title"><a href="#">Sản phẩm đã xem</a></h4>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h4 class="panel-title"><a href="#">Nhận xét sản phẩm đã mua</a></h4>
+			</div>
+		</div>
+		
+	</div><!--/category-products-->
+</div>
